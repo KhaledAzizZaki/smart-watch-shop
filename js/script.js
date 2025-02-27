e1631f25e1721e89a3e0c155c1c01f4b1c6c62c0
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-27 23:31:14 +0600

    add quantity

--- js/script.js
+++ js/script.js
@@ -31,3 +31,18 @@ for (let i of sizeBtn) {
     event.target.classList.add("border-purple-600");
   });
 }
+
+// Quantity
+let quantityButton = document.querySelectorAll(".quantity-button");
+let sum = 0;
+for (let i of quantityButton) {
+  i.addEventListener("click", function (event) {
+    if (event.target.innerText == "+") {
+      sum += 1;
+      document.getElementById("quantity").innerText = sum;
+    } else {
+      sum = Math.max(0, (sum -= 1));
+      document.getElementById("quantity").innerText = sum;
+    }
+  });
+}
